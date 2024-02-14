4ea4a488e37293ff12874811152dcac35e843ec3
davidfeld <davidfeld@hilma.tech>
2024-02-14 12:29:57 +0200

    revert(app): removed provider from app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Todos } from "./components/Todos";
-
-const client = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={client}>
+    <>
       <h1>TODOs</h1>
-
       <Todos />
-
-    </QueryClientProvider>
+    </>
   );
 }
 
